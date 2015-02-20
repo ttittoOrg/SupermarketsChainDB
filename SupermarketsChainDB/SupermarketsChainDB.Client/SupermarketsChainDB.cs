0751c03e5cd27e0073470e72325b0c62ea5a45ef
ttitto <ttitto_prog@vfemail.net>
2015-02-20 11:14:30 +0200

    Console app renamed

---
+++ SupermarketsChainDB/SupermarketsChainDB.Client/SupermarketsChainDB.cs
@@ -0,0 +1,15 @@
+﻿namespace SupermarketsChainDB.Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SupermarketsChainDB
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
