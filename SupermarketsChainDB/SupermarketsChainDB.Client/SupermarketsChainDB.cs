c0b197949af5fa1941048d9574df13982b6dcbe4
ttitto <ttitto_prog@vfemail.net>
2015-03-11 22:11:20 +0200

    working version with custom conn. string

--- SupermarketsChainDB/SupermarketsChainDB.Client/SupermarketsChainDB.cs
+++ SupermarketsChainDB/SupermarketsChainDB.Client/SupermarketsChainDB.cs
@@ -12,33 +12,34 @@
     using SupermarketsChainDB.Data.Migrations;
     using SupermarketsChainDB.Data;
 
-    public class SupermarketsChainDBConsoleClient
+    public class SupermarketsChainDBClient
     {
 
         static void Main(string[] args)
         {
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<SupermarketSystemDbContext, Configuration>());
             var data = new SupermarketSystemData();
+            var measures = data.Measures.All().ToList();
 
-            String ConnStr = "Server={0};Database=SupermarketsChainDB;User Id=yavor;Password=Limon123;";
+            //String ConnStr = "Server=(localdb)v11.0;Database=SupermarketSystem;Integrated Security=True;";
 
-            String database = "192.168.137.156";
+            //String database = "192.168.137.156";
 
-            ConnStr = String.Format(ConnStr, database);
+            //ConnStr = String.Format(ConnStr, database);
 
-            SqlManagerConnection sqlManager = new SqlManagerConnection(ConnStr);
-
-
-            DateTime fromDate = DateTime.ParseExact("20.07.2014", "dd.MM.yyyy", CultureInfo.InvariantCulture);
-            DateTime toDate = DateTime.ParseExact("22.07.2014", "dd.MM.yyyy", CultureInfo.InvariantCulture);
-            List<Product> p = sqlManager.GetProducts(fromDate, toDate);
+            //SqlManagerConnection sqlManager = new SqlManagerConnection(ConnStr);
 
 
-            foreach (Product item in p)
-            {
-                Console.WriteLine(item.Sum);
-                Console.WriteLine(item.Quantity);
-            }
+            //DateTime fromDate = DateTime.ParseExact("20.07.2014", "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            //DateTime toDate = DateTime.ParseExact("22.07.2014", "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            //List<Product> p = sqlManager.GetProducts(fromDate, toDate);
+
+
+            //foreach (Product item in p)
+            //{
+            //    Console.WriteLine(item.Sum);
+            //    Console.WriteLine(item.Quantity);
+            //}
 
         }
     }
